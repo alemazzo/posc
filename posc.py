7305c3e5f8f40373b98a4dc6d0a1851c7fbd825b
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-07-02 01:23:34 +0200

    refactored project structure

--- posc.py
+++ posc.py
@@ -1,32 +1,39 @@
 #!/bin/python3
+# Moduels import
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
 
-from posc.arg_parser import getParser
-parser = getParser()
-parser.parse_args()
-
-# ...
-stream = open('config.yml')
-data = load(stream, Loader=Loader)
+# My imports
+from posc.arguments.parser import parse_arguments
 
 
-# Load directories
-directories = data["directories"]
-print(f"Directories = {directories}")
+if __name__ == "__main__":
+    args = parse_arguments()
+    file = args.file
+    print(file)
 
-# Load applications
-applications = data["applications"]
-apt_applications = applications["apt"]
-snap_applications = applications["snap"]
-flatpak_applications = applications["flatpak"]
-print(f"apt = {apt_applications}")
-print(f"snap = {snap_applications}")
-print(f"flatpak = {flatpak_applications}")
 
-# Load tasks
-tasks = data["tasks"]
-print(f"tasks = {tasks}")
+def execute():
+    # ...
+    stream = open('config.yml')
+    data = load(stream, Loader=Loader)
+
+    # Load directories
+    directories = data["directories"]
+    print(f"Directories = {directories}")
+
+    # Load applications
+    applications = data["applications"]
+    apt_applications = applications["apt"]
+    snap_applications = applications["snap"]
+    flatpak_applications = applications["flatpak"]
+    print(f"apt = {apt_applications}")
+    print(f"snap = {snap_applications}")
+    print(f"flatpak = {flatpak_applications}")
+
+    # Load tasks
+    tasks = data["tasks"]
+    print(f"tasks = {tasks}")
--- posc/arguments/parser.py
+++ posc/arguments/parser.py
@@ -1,12 +1,18 @@
 import argparse
 
 
-def getParser():
+def get_argument_parser():
 
     parser = argparse.ArgumentParser(
         description='Setup your OS in one command')
 
-    parser.add_argument('config',
+    parser.add_argument('file',
                         help='YAML configuration file')
 
     return parser
+
+
+def parse_arguments():
+    parser = get_argument_parser()
+    args = parser.parse_args()
+    return args
